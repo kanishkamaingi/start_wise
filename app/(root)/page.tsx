4b9217e25fd3c2881093bf4d37450de3b1f6ae11
kanishka <kanishkamaingi21@gmail.com>
2025-02-17 01:21:01 -0500

    searchbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,16 @@
+import SearchForm from "../components/SearchForm";
+
 export default function Home(){
   return(
     <>
-      <h1 className="text-2xl">HOME</h1>
+      <section className="pink_container">
+        <h1 className="heading">Pitch your startup, <br /> Connect with entrepreneurs</h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and get noticed in virtual competitions.
+        </p>
+
+        <SearchForm />
+      </section>
       
     </>
   )
