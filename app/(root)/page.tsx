828d107a09d85f60353c6fa10b9f4674fd3d6f29
kanishka <kanishkamaingi21@gmail.com>
2025-02-25 22:59:07 -0500

    startup card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,22 @@
+import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
 
 export default async function Home({searchParams} : {
   searchParams : Promise<{ query?: string }>
 }){
   const query = (await searchParams).query
+
+  const posts = [{
+    _createdAt : 'Yesterday',
+    views : 55,
+    author : {_id : 1},
+    _id : 1,
+    description : 'A marketplace for sustainable and eco-friendly products.',
+    image : "https://plus.unsplash.com/premium_photo-1661368421663-13b2d8115241?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZWNvJTIwZnJpZW5kbHl8ZW58MHx8MHx8fDA%3D",
+    category : 'Nature',
+    title : 'EcoNest'
+
+  }]
 
   return(
     <>
@@ -13,7 +26,25 @@ export default async function Home({searchParams} : {
           Submit Ideas, Vote on Pitches, and get noticed in virtual competitions.
         </p>
 
+
         <SearchForm query = {query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "$query"` : `All Startups`}
+        </p>
+        <ul className="mt-7 card_grid">
+          {
+            posts?.length > 0 ? (
+              posts.map((post : StartupCardType, index : number) => (
+                <StartupCard key = { post?._id} post = {post  }/>
+              ))
+            ) : (
+              <p className="no-results">No startups found</p>
+            )
+          }
+        </ul>
       </section>
       
     </>
---
+++ components/StartupCard.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const StartupCard = ({post} : {post : StartupTypeCard}) => {
+    return (
+        <li className="startup-card group">
+            <div className="flex-group">
+                <p className="startup_card_date">{post._createdAt}</p>
+            </div>
+
+        </li>
+    )
+}
+
+export default StartupCard
