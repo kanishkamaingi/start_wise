cb1803416380a5805d24631f4455dcde84283367
kanishka <kanishkamaingi21@gmail.com>
2025-02-27 00:25:51 -0500

    startupcard

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -7,9 +7,9 @@ export default async function Home({searchParams} : {
   const query = (await searchParams).query
 
   const posts = [{
-    _createdAt : 'Yesterday',
+    _createdAt : new Date(),
     views : 55,
-    author : {_id : 1},
+    author : {_id : 1, name: "Adrian"},
     _id : 1,
     description : 'A marketplace for sustainable and eco-friendly products.',
     image : "https://plus.unsplash.com/premium_photo-1661368421663-13b2d8115241?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8ZWNvJTIwZnJpZW5kbHl8ZW58MHx8MHx8fDA%3D",
