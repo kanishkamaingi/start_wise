3fb51c82dbdf1b849d0f375a099fd8654370cc4c
kanishka <kanishkamaingi21@gmail.com>
2025-03-06 23:39:12 -0500

    added startups

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,23 +1,35 @@
+
 import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import React from "react";
 
-const Page = async ({params} : {
-    params: Promise<{id : string}>}) => {
+// export default async function Home({searchParams} : {
+//     searchParams : Promise<{ query?: string }>
+//   }){
+//     const query = (await searchParams).query
+//     const params = {search : query || null }
+    
+//     const {data : posts} = await sanityFetch({query: STARTUP_QUERY, params}) }
+    
+
+export default async function Page({params} : {
+    params: {id : string}}) {
     const id = (await params).id
+    console.log("Sanity client:", client);
+    console.log("Fetching post with ID:", id);
+
     const post = await client.fetch(STARTUP_BY_ID_QUERY, {id})
+    console.log("post stored:", post)
     if (!post) return notFound();
 
     return (
         <>
         <section className="pink_container !min-h-[230px]">
-            <p className="tag">{formatDate(post?._createdAt)}</p>
-            <h1 className="heading">{post.title}</h1>
+            hi
         </section>
         </>
     )
 }
 
-export default Page 
